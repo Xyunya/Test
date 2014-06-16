3bb198524a11bb4966c7ec3b4a5b1466e1e84dab
Naits00 <kol2160@mail.ru>
2014-06-16 21:06:14 +0400

    Pasha Lo

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -7,14 +7,7 @@ public class JavaApplication7 {
 
  
     public static void main(String[] args) {
-//лааах //  void
-      //     gdgdsgsdggdsfs
 
-        
-    //    asufgasgfiasufiuasdf
-
-        //blablabla
-     KHSGDlhasgfsdbf;sdk/lsgm;ldfsgmk;ldfmhd;ofhjmdphlfhgdhdgh
     }
     
 }
